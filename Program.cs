617161ecdcc00085f0451cd536e4588ffbba7b30
jazmynnbanks <jazmynn.lee@outlook.com>
2025-06-13 17:01:20 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿List<int> numbers = new List<int> { 55, 200, 100, 76, 230, 65, 95 };
+
+Console.WriteLine("The numbers greater than 80 are:");
+
+foreach (int number in numbers)
+{
+    if (number > 80)
+    {
+        Console.WriteLine(number);
+    }
+}
+    
